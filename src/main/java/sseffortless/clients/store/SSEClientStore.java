30a8908815e2f33c3ad8e581d168d6041a8774e7
Ilias Dewachter <ilias.dewachter@telenet.be>
2019-03-30 19:29:38 +0100

    Added SSEClientStore

---
+++ src/main/java/sseffortless/clients/store/SSEClientStore.java
@@ -0,0 +1,37 @@
+package sseffortless.clients.store;
+
+import sseffortless.clients.client.SSEClient;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public final class SSEClientStore<K> {
+
+    private Map<K, SSEClient> clientStore = new HashMap<>();
+
+    public void register(K key, SSEClient client) {
+        if (this.isRegistered(key)) {
+            throw new IllegalStateException(String.format("Key %s is already registered.", key.toString()));
+        }
+        if (this.isRegistered(client)) {
+            throw new IllegalStateException("Client is already registered.");
+        }
+        clientStore.put(key, client);
+    }
+
+    public boolean unregister(K key) {
+        return clientStore.remove(key) != null;
+    }
+
+    public boolean isRegistered(K key) {
+        return clientStore.containsKey(key);
+    }
+
+    public boolean isRegistered(SSEClient client) {
+        return clientStore.containsValue(client);
+    }
+
+    public void unregisterAll() {
+        clientStore.clear();
+    }
+}
